7cf3f27701fe557903c1d178725a26f5a0968765
Iemon-creator <markuscrib@gmail.com>
2026-03-25 15:49:13 +0800

    Update Mathematical Constants

--- mathconstants.js
+++ mathconstants.js
@@ -1,7 +1,8 @@
-class Mathconst {
+
+Scratch.extensions.register(new class {
   getInfo() {
     return {
-      id: 'mathconstants',
+      id: 'lemonmathconstants',
       name: 'Mathematical Constants',
       blocks: [
         {
@@ -38,15 +39,29 @@ class Mathconst {
               text: "γ", value: '0.5772156649015329'
             },
             {
-              text: 'Ω', value: '0.5671432904097838'
+              text: "β", value: '1.1865691104156255'
+            },
+            {
+              text: 'C₁', value: '0.697774657964008'
+            },
+            {
+              text: 'ρ', value: '1.324717957244746'
             },
             {
               text: 'P', value: '2.295587149392638'
+            },
+            {
+              text: 'θₘ', value: '0.9553166181245093'
+            },
+            {
+              text: 'Δ(3)', value: '0.6617071822671763'
+            },
+            {
+              text: 'ζ(3)', value: '1.2020569031595942'
             }
           ]
         }
       }
     };
   }
-}
-Scratch.extensions.register(new Mathconst());
+});
